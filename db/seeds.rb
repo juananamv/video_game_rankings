b23f353062230e482a7db9a6ec2454d42c7d1599
Juan Villarin <villjua96@gmail.com>
2020-10-09 19:05:51 -0500

    seed script to import data from csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+VideoGameIteration.delete_all
+VideoGameTitle.delete_all
+Publisher.delete_all
+Genre.delete_all
+Platform.delete_all
+
+filename = Rails.root.join("db/VG_Sales_Multi.csv")
+
+video_game_data = File.read(filename)
+
+video_games = CSV.parse(video_game_data, headers: true, encoding: "utf-8")
+
+video_games.each do |v|
+  publisher = Publisher.find_or_create_by(name: v["Publisher"])
+  genre = Genre.find_or_create_by(name: v["Genre"])
+
+  video_game_title = publisher.video_game_titles.find_or_create_by(
+    name: v["Name"],
+    genre: genre
+  )
+
+  platform = Platform.find_or_create_by(name: v["Platform"])
+
+  video_game_iteration = video_game_title.video_game_iterations.create(
+    year: v["Year_of_Release"],
+    sales: v["Global_Sales"],
+    rating: v["Critic_Score"],
+    platform: platform
+  )
+end
+
+puts "Generated #{Publisher.count} Publishers"
+puts "Generated #{Genre.count} Genres"
+puts "Generated #{Platform.count} Platforms"
+puts "Generated #{VideoGameTitle.count} Video Game Titles"
+puts "Generated #{VideoGameIteration.count} Video Game Iterations"
