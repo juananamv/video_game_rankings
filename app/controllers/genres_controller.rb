6beb355f25365dcf78051ca3a3bd4ef28daf76f1
Juan Villarin <villjua96@gmail.com>
2020-10-11 14:19:03 -0500

    updated genre to follow new format

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,4 +2,8 @@ class GenresController < ApplicationController
   def index
     @genres = Genre.includes(:video_game_titles).order("name ASC")
   end
+
+  def show
+    @genre = Genre.includes(:video_game_titles).find(params["id"])
+  end
 end
